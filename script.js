235fecb0cac5802cba05d90b712e6b6061229a78
nikhil9206 <nikhiladapala@icloud.com>
2024-06-21 10:37:35 -0500

    finished profile page + fixed scrolling for all pages

--- script.js
+++ script.js
@@ -126,15 +126,31 @@ for (i = 0; i < 15; i++) {
 /* ################################################################################################################################ */
 /* ################################################################################################################################ */
 
+//description function
 var savedDescription = '';
 
-function saveDescription(descriptionId) {
-    var description = document.getElementById(descriptionId);
+function saveDescription(id) {
+    var description = document.getElementById(id);
     savedDescription = description.value;
     description.textContent = description.value;
 }
 
 function resetDescription() {
     var description = document.getElementById('description-profile');
-    description.value = savedDescription;
+    description.value = savedDescription; // Reset to the saved description
+}
+
+
+//expand guesses function
+function expand(evt, id) {
+    var item = document.getElementById(id);
+    var button = evt.currentTarget;
+    
+    if (item.classList.contains('active')) {
+        item.classList.remove('active');
+        button.classList.remove('active');
+    } else {
+        item.classList.add('active');
+        button.classList.add('active');
+    }
 }
