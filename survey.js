22dec3e822d6ba75f000924ef7a97d44721c284f
nikhil9206 <nikhiladapala@icloud.com>
2024-06-22 17:57:23 -0500

    survey edits

--- survey.js
+++ survey.js
@@ -23,6 +23,62 @@ function next(id) {
     setTimeout(function() {
         nextPage.classList.remove('go-right');
         nextPage.classList.add('fade-in');
-        nextPage.classList.remove('faded-out');
     }, 500);
+
+    setTimeout(function() {
+        nextPage.classList.remove('fade-in');
+    }, 1000);
 }
+
+function back(id) {
+    var page = document.getElementById(id);
+    var prevPage;
+
+    // Fade out current page to the right
+    page.classList.add('go-right');
+
+    setTimeout(function() {
+        page.classList.remove('active');
+        page.classList.remove('go-right');
+    }, 500);
+
+    // Find previous page
+    for (var i = 0; i < pages.length; i++) {
+        if (pages[i] === id) {
+            prevPage = document.getElementById(pages[i - 1]);
+            break;
+        }
+    }
+
+    // Fade in previous page from the left
+    prevPage.classList.add('go-left', 'active');
+
+    setTimeout(function() {
+        prevPage.classList.remove('go-left');
+        prevPage.classList.add('fade-in');
+    }, 500);
+
+    setTimeout(function() {
+        prevPage.classList.remove('fade-in');
+    }, 1000);
+}
+
+
+
+var buttonsAge = ['age1', 'age2', 'age3', 'age4', 'age5', 'age6', 'age7']
+
+function select(evt) {
+    var buttonPressed = evt.currentTarget;
+    
+    // Remove 'active' class from all buttons
+    for (var i = 0; i < buttonsAge.length; i++) {
+        var button = document.getElementById(buttonsAge[i])
+
+        if (button) {
+            button.classList.remove('active')
+        }
+    }
+    // Add 'active' class to the clicked button
+    buttonPressed.classList.toggle('active');
+}
+
