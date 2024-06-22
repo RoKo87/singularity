cfb2319f9fc8e3afd8daf82950e2d64ec429b952
nikhil9206 <nikhiladapala@icloud.com>
2024-06-21 19:28:55 -0500

    basic survey interactivity

--- survey.js
+++ survey.js
@@ -1,15 +1,16 @@
-var pages = ['main', 'age', 'location', 'occupation', 'education']
+var pages = ['main', 'age', 'location', 'occupation', 'education'];
 
 function next(id) {
     var page = document.getElementById(id);
-    page.classList.remove('active')
+    var nextPage;
 
+    page.classList.add('go-left');
+    
     setTimeout(function() {
-        page.style.display = 'none'
-        console.log("Waited for 0.5 seconds");
+        page.classList.remove('active');
+        page.classList.remove('go-left');
     }, 500);
 
-    var nextPage;
     for (var i = 0; i < pages.length; i++) {
         if (pages[i] === id) {
             nextPage = document.getElementById(pages[i + 1]);
@@ -17,15 +18,11 @@ function next(id) {
         }
     }
 
-    if (nextPage) {
-        nextPage.classList.add('active')
-        setTimeout(function() {
-            nextPage.style.display = 'block'
-            console.log("Waited for 0.5 seconds");
-        }, 500);
-    } else {
-        // Handle case where nextPage is not found (end of pages array)
-        console.log('No next page found.');
-    }
-}
+    nextPage.classList.add('go-right', 'active');
 
+    setTimeout(function() {
+        nextPage.classList.remove('go-right');
+        nextPage.classList.add('fade-in');
+        nextPage.classList.remove('faded-out');
+    }, 500);
+}
